46b1dede1f820c2fad654e2eada022684ef9961f
Vital D'haveloose <vital.dhaveloose@imec.be>
2021-11-30 15:42:07 +0100

    PHR-112: converted 4_creatinine.sql to query eICU DB + added codes.md to document code conversions

---
+++ codes.md
@@ -0,0 +1,176 @@
+# codes
+
+Converted codes
+
+| sql file         | mimic III table | mimic III column | eICU table | eICU column | mimic III code(s) | eICU code(s) |
+| ---------------- | --------------- | ---------------- | ---------- | ----------- | ----------------- | ------------ |
+| 4_creatinine.sql | labevents       | ITEMID           | lab        | labName     | 50912             | creatinine   |
+
+
+
+
+
+
+## eICU lab#labName values
+
+`SELECT DISTINCT labName FROM eicu_crd.lab ORDER BY labName ASC`
+
+=-bands
+=-basos
+=-eos
+=-lymphs
+=-monos
+=-polys
+24 h urine protein
+24 h urine urea nitrogen
+Acetaminophen
+albumin
+alkaline phos.
+ALT (SGPT)
+Amikacin - peak
+Amikacin - random
+Amikacin - trough
+ammonia
+amylase
+ANF/ANA
+anion gap
+AST (SGOT)
+Base Deficit
+Base Excess
+bedside glucose
+bicarbonate
+BNP
+BUN
+calcium
+Carbamazepine
+Carboxyhemoglobin
+cd 4
+chloride
+Clostridium difficile toxin A+B
+cortisol
+CPK
+CPK-MB
+CPK-MB INDEX
+creatinine
+CRP
+CRP-hs
+Cyclosporin
+Device
+Digoxin
+direct bilirubin
+ESR
+ethanol
+Fe
+Fe/TIBC Ratio
+Ferritin
+fibrinogen
+FiO2
+folate
+free T4
+Gentamicin - peak
+Gentamicin - random
+Gentamicin - trough
+glucose
+glucose - CSF
+haptoglobin
+HCO3
+Hct
+HDL
+Hgb
+HIV 1&2 AB
+HSV 1&2 IgG AB
+HSV 1&2 IgG AB titer
+ionized calcium
+lactate
+LDH
+LDL
+Legionella pneumophila Ab
+Lidocaine
+lipase
+Lithium
+LPM O2
+magnesium
+MCH
+MCHC
+MCV
+Methemoglobin
+Mode
+MPV
+myoglobin
+NAPA
+O2 Content
+O2 Sat (%)
+Oxyhemoglobin
+paCO2
+paO2
+Peak Airway/Pressure
+PEEP
+pH
+Phenobarbital
+Phenytoin
+phosphate
+platelets x 1000
+potassium
+prealbumin
+Pressure Control
+Pressure Support
+Procainamide
+prolactin
+protein - CSF
+protein C
+protein S
+PT
+PT - INR
+PTT
+PTT ratio
+RBC
+RDW
+Respiratory Rate
+reticulocyte count
+RPR titer
+salicylate
+serum ketones
+serum osmolality
+Site
+sodium
+Spontaneous Rate
+T3
+T3RU
+T4
+Tacrolimus-FK506
+Temperature
+Theophylline
+TIBC
+Tobramycin - peak
+Tobramycin - random
+Tobramycin - trough
+total bilirubin
+total cholesterol
+Total CO2
+total protein
+transferrin
+triglycerides
+troponin - I
+troponin - T
+TSH
+TV
+uric acid
+urinary creatinine
+urinary osmolality
+urinary sodium
+urinary specific gravity
+Vancomycin - peak
+Vancomycin - random
+Vancomycin - trough
+Vent Other
+Vent Rate
+Vitamin B12
+WBC x 1000
+WBC's in body fluid
+WBC's in cerebrospinal fluid
+WBC's in pericardial fluid
+WBC's in peritoneal fluid
+WBC's in pleural fluid
+WBC's in synovial fluid
+WBC's in urine
+
--- sql/eicu/4_creatinine.sql
+++ sql/eicu/4_creatinine.sql
@@ -1,16 +1,17 @@
 DROP MATERIALIZED VIEW IF EXISTS kdigo_creat CASCADE;
-CREATE MATERIALIZED VIEW kdigo_creat as with cr as (
-    select ie.icustay_id,
-        ie.intime,
-        ie.outtime,
-        le.valuenum as creat,
-        le.charttime
-    from icustays ie
-        left join labevents le on ie.subject_id = le.subject_id
-                                  and le.ITEMID = 50912 -- Creatinine,Creatinine,,verify,,50912,CREATININE,,labevents,797476,CHEMISTRY,,BLOOD,2160-0,,,,
-                                  and le.VALUENUM is not null
-                                  and le.CHARTTIME between (ie.intime - interval '7' day)
-                                                       and (ie.intime + interval '7' day)
+CREATE MATERIALIZED VIEW kdigo_creat AS
+WITH cr as (
+    select ie.patientUnitStayID AS icustay_id,
+        --ie.intime,  actually not used
+        --ie.outtime, actually not used
+        le.labResult AS creat,
+        le.labResultOffset - anyStayOfPat.hospitalAdmitOffset AS charttime -- charttime is relative to hospital admin (in minutes), which is okay as long as compared only with charttimes of the same patient/hospital stay
+    from patient ie
+         left join patient anyStayOfPat on ie.patientHealthSystemStayID = anyStayOfPat.patientHealthSystemStayID -- in mimic we can group data on a patient level, here we can only group on a hospital stay level
+         left join lab le on anyStayOfPat.patientUnitStayID = le.patientUnitStayID -- mimic doesn't associate lab events to unit stays, so we also select lab data from all stays of the patient
+                             and le.labName = 'creatinine'
+                             and le.labResult is not null
+                             and le.labResultOffset - anyStayOfPat.hospitalAdmitOffset between -7*24*60-ie.hospitalAdmitOffset AND 7*24*60-ie.hospitalAdmitOffset -- correct for the time difference (base for the labResultOffset) between the 'ie' stay and anyStayOfPat
 )
 SELECT cr.icustay_id,
     cr.charttime,
@@ -19,10 +20,10 @@ SELECT cr.icustay_id,
     MIN(cr7.creat) AS creat_low_past_7day
 FROM cr
     LEFT JOIN cr cr48 ON cr.icustay_id = cr48.icustay_id
-                         AND cr48.charttime < cr.charttime
-                         AND cr48.charttime >= (cr.charttime - INTERVAL '48' HOUR)
+                         AND cr48.charttime < cr.charttime --TODO this results in null values in the column creat_low_past_48hr for the rows where cr.creat is the lowest value for the unit stay, is this correct behaviour?
+                         AND cr48.charttime >= (cr.charttime - 48*60)
     LEFT JOIN cr cr7 ON cr.icustay_id = cr7.icustay_id
-                        AND cr7.charttime < cr.charttime
-                        AND cr7.charttime >= (cr.charttime - INTERVAL '7' DAY)
+                        AND cr7.charttime < cr.charttime --TODO this results in null values in the column creat_low_past_7day for the rows where cr.creat is the lowest value for the unit stay, is this correct behaviour?
+                        AND cr7.charttime >= (cr.charttime - 7*24*60)
 GROUP BY cr.icustay_id, cr.charttime, cr.creat
 ORDER BY cr.icustay_id, cr.charttime, cr.creat;
